c0b51c78090e02cbd5486307468d338847739a43
KYJ <rdw7793@gmail.com>
2021-12-22 11:42:32 +0900

    21년 12월 22일 selectOneMember method 추가

--- src/kr/or/artsuwon/member/controller/MemberLoginServlet.java
+++ src/kr/or/artsuwon/member/controller/MemberLoginServlet.java
@@ -1,11 +1,15 @@
 package kr.or.artsuwon.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.or.artsuwon.member.model.service.MemberService;
+import kr.or.artsuwon.member.model.service.MemberServiceImpl;
 
 /**
  * Servlet implementation class MemberLoginServlet
@@ -26,9 +30,11 @@ public class MemberLoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
 		
-		
-		
+		MemberService mService = new MemberServiceImpl();
+		mService.selectOneMember(userId, userPwd);
 		
 		
 		
--- src/kr/or/artsuwon/member/model/service/MemberService.java
+++ src/kr/or/artsuwon/member/model/service/MemberService.java
@@ -1,5 +1,17 @@
 package kr.or.artsuwon.member.model.service;
 
+import kr.or.artsuwon.member.model.vo.Member;
+
 public interface MemberService {
+
+	/**
+	 * 작성자 : 김영주
+	 * 작성일 : 2021.12.22
+	 * @param userId
+	 * @param userPwd
+	 * @return Member
+	 * Description : 회원 로그인 로직
+	 */
+	public Member selectOneMember(String userId, String userPwd);
 
 }
