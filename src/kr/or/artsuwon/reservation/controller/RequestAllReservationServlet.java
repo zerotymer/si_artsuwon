506f27b8eac33f1bea5c88d6d278dd4e09794251
zerotymer <zerotymer+github@gmail.com>
2021-12-27 16:08:31 +0900

    2021-12-27 오후 4:08:22 | pull 전 commit

---
+++ src/kr/or/artsuwon/reservation/controller/RequestAllReservationServlet.java
@@ -0,0 +1,56 @@
+package kr.or.artsuwon.reservation.controller;
+
+import com.google.gson.Gson;
+import kr.or.artsuwon.reservation.model.service.ReservationService;
+import kr.or.artsuwon.reservation.model.service.ReservationServiceImpl;
+import kr.or.artsuwon.reservation.model.vo.Reservation;
+import org.json.simple.JSONArray;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestAllReservationServlet
+ */
+@WebServlet("/reservation/requestAll.do")
+public class RequestAllReservationServlet extends HttpServlet {
+	/// FIELDs
+	private static final long serialVersionUID = 1L;
+	private ReservationService service = new ReservationServiceImpl();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestAllReservationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Parameters
+		int scheduleNo = Integer.parseInt(request.getParameter("scheduleNo"));
+
+		// Business Logic
+		ArrayList<Reservation> list = service.getAllReservationByPerfSchedule(scheduleNo);
+
+
+		new Gson().toJson(list, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/kr/or/artsuwon/reservation/model/service/ReservationService.java
+++ src/kr/or/artsuwon/reservation/model/service/ReservationService.java
@@ -1,5 +1,15 @@
 package kr.or.artsuwon.reservation.model.service;
 
-public interface ReservationService {
+import kr.or.artsuwon.reservation.model.vo.Reservation;
 
+import java.util.ArrayList;
+
+public interface ReservationService {
+    /**
+     * 모든 예약 리스트를 반환하는 메소드
+     * @param scheduleNo 공연일정
+     * @return 예약 리스트
+     * @author 신현진
+     */
+    ArrayList<Reservation> getAllReservationByPerfSchedule(int scheduleNo);
 }
